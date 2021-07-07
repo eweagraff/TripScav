2b2c74cf999b96c64e5754835b7b22ead138c373
eweagraff <weagraff.emily@gmail.com>
2021-07-07 15:26:20 -0700

    added login/logout files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,32 @@
+const router = require('express').Router();
+const { Event, User, Location } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    // Get all events and JOIN with user data
+    const eventData = await Event.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const events = eventData.map((event) => event.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      events, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //const router = require('express').Router();
 
 //router.get("/", (req, res) => {

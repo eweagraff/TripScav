d782a5fea43706ac50dd4eb7c006184e2bafec75
Veikko Aho <vidfinnx@yahoo.com>
2021-07-10 10:42:28 -0700

    troublshooting

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-Routes.js');
+const homeRoutes = require('./home-routes.js');
 
 
 router.use('/', homeRoutes);

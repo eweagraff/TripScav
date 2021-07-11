cf9a9197aa0734c3211f64817d5217f3892d994f
eweagraff <weagraff.emily@gmail.com>
2021-07-11 06:53:21 -0700

    added searchh bar/search button directed to api call

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,39 +1,34 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Site, User } = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Site, User } = require("../models");
 // const withAuth = require('../utils/auth');
 
-
-
-
 //Starts us on homepage by default
-router.get('/', (req, res) => {
-  res.render('homepage', {
+router.get("/", (req, res) => {
+  res.render("homepage", {
     loggedIn: req.session.loggedIn,
   });
 });
 
-
 // Goes to Login page when click Login If logged in redirect to path and stop function otherwise render the login.handlebars template form to body
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   console.log("\n=======================================");
   console.log("\n++++++++++++LOGIN CLICKED ON HOMEPAGE+++++++++++++");
   console.log("\n=======================================");
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
-  res.render('login');
+  res.render("login");
 });
 
 // Renders signup.handlebars template form to main.handlebars body
-router.get('/signup', (req, res) => {
+router.get("/signup", (req, res) => {
   console.log("\n=======================================");
   console.log("\n++++++++++++SIGN UP CLICKED ON HOMEPAGE+++++++++++++");
   console.log("\n=======================================");
-  res.render('signup');
+  res.render("signup");
 });
-
 
 // router.get('/', withAuth, async (req, res) => {
 //   try {
@@ -51,17 +46,17 @@ router.get('/signup', (req, res) => {
 //     const events = siteData.map((event) => event.get({ plain: true }));
 
 //     // Pass serialized data and session flag into template
-//     res.render('homepage', { 
-//       events, 
-//       logged_in: req.session.logged_in 
+//     res.render('homepage', {
+//       events,
+//       logged_in: req.session.logged_in
 //     });
 //   } catch (err) {
 //     res.status(500).json(err);
 //   }
 // });
 
-router.get('/results', (req, res) => {
-  res.render('script')
-})
+router.get("/script", (req, res) => {
+  res.render("script");
+});
 
 module.exports = router;

42167b31d4b9a0ad45b3656328070c345c8198df
Edgar Lule <edgar21lc@gmail.com>
2021-07-12 16:30:59 -0700

    added so any country can be searched

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+require("dotenv").config();
 //const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 
@@ -37,5 +38,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening on ' + PORT));
+  app.listen(PORT, () => console.log(`Now listening on PORT: ${PORT}`));
 });

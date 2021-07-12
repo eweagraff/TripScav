ba834162f2116029876d1f43d1cccadfb614e261
Veikko Aho <vidfinnx@yahoo.com>
2021-07-12 13:08:27 -0700

    MODALS NOW WILL ONLY CLOSE WHEN USING THE X CLOSE BUTTON

--- public/js/menu.js
+++ public/js/menu.js
@@ -13,54 +13,54 @@ function openMore(n) {
     //Get elements with class="modal" into an array
     var modal = document.getElementsByClassName("modal");
     //Change style of modal number [n] to display = "block"
-    modal[n].classList.toggle("is-active");
+    modal[n].classList.add("is-active");
     html.classList.add('is-clipped');
 }
 
-//This will close the modal once you click on it
-window.onclick = function(event) {
-    
+//This will close the modal once you click on the X
+window.onclick = function (event)  {
+    console.log(event.target);
 
-    //For multiple modal closures
-    var modal = document.getElementsByClassName("modal")
-    for ( var i = 0; i <modal.length; i++)
-    //If the click was on the modal the modal class "is-active" removed
-    if (event.target.parentElement == modal[i]) {
-        modal[i].classList.remove ("is-active");
+        //For multiple modal closures
+        var modal = document.getElementsByClassName("modal-close")
+        for (var i = 0; i <= modal.length; i++)
+            //If the click was on the modal the modal class "is-active" removed
+            if (event.target == modal[i]) {
+                modal[i].parentElement.classList.remove("is-active");
+            }
     }
-}
 
 
-// Millenium Falcon + Tie Fighter Cursor Flying
-var cursor = document.querySelector(".cursor");
-var cursor2 = document.querySelector(".cursor2");
-document.addEventListener("mousemove",function(e){
-  cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
-});
+    // Millenium Falcon + Tie Fighter Cursor Flying
+    var cursor = document.querySelector(".cursor");
+    var cursor2 = document.querySelector(".cursor2");
+    document.addEventListener("mousemove", function (e) {
+        cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+    });
 
 
-// Burger menu
-document.addEventListener('DOMContentLoaded', () => {
+    // Burger menu
+    document.addEventListener('DOMContentLoaded', () => {
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
 
-        // Add a click event on each of them
-        $navbarBurgers.forEach(el => {
-            el.addEventListener('click', () => {
+            // Add a click event on each of them
+            $navbarBurgers.forEach(el => {
+                el.addEventListener('click', () => {
 
-                // Get the target from the "data-target" attribute
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
+                    // Get the target from the "data-target" attribute
+                    const target = el.dataset.target;
+                    const $target = document.getElementById(target);
 
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
+                    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                    el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
 
+                });
             });
-        });
-    }
-});
+        }
+    });

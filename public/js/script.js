d560314dd1d67b0ade47736e30740d69dcdee9e6
Edgar Lule <edgar21lc@gmail.com>
2021-07-13 22:07:04 -0700

    added the rest country api

--- public/js/script.js
+++ public/js/script.js
@@ -249,34 +249,65 @@
 //////////////////////////////// TRYING TO USE AXIOS ////////////////////////////////
 // var axios = require("axios");
 
+// var axios = require('axios');
+// let countryCodeLower;
+let countryCode;
+
 function search() {
-  let searchText = document.getElementById("search-bar").value.toLowerCase();
+  let searchText = document.getElementById("search-bar").value;
 
   var config = {
     method: "get",
-    url: `https://www.triposo.com/api/20210615/poi.json?countrycode=${searchText}&order_by=-score&count=10&fields=images,attribution,location_id,name,score,location_ids,snippet,intro`,
-    headers: {
-      "X-Triposo-Account": "YU3ML92G",
-      "X-Triposo-Token": "6ny9va4c72n2xj5b9sw7wpjlubr7uitv",
-    },
+    url: `https://restcountries.eu/rest/v2/name/${searchText}`,
+    headers: {},
   };
 
   axios(config)
     .then(function (response) {
-      console.log(response.data);
-      const html = response.data.results.map((event) => {
-          // rounds down the rating
-          let score = event.score;
-          let roundedScore = score.toFixed(2);
-          // uses regex to remove uncessary text from city names
-          let city = event.location_id;
-          if (city) {
-            city = city.replace("2C_", " ");
-            city = city.replace(/_/g, " ");
-            city = city.replace(/-/g, " ");
-            city = city.replace(/[0-9]/g, " ");
-          }
-          return `
+      // console.log(response.data);
+      console.log(response.data[0].alpha2Code.toLowerCase());
+      countryCode = response.data[0].alpha2Code.toLowerCase();
+      // countryCodeLower = countryCode.toLowerCase();
+      // console.log(countryCodeLower);
+      input()
+      // return countryCodeLower;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+// search();
+
+
+function input () {
+  let code = countryCode;
+
+var config = {
+  method: "get",
+  url: `https://www.triposo.com/api/20210615/poi.json?countrycode=${code}&order_by=-score&count=10&fields=images,attribution,location_id,name,score,location_ids,snippet,intro`,
+  headers: {
+    "X-Triposo-Account": "YU3ML92G",
+    "X-Triposo-Token": "6ny9va4c72n2xj5b9sw7wpjlubr7uitv",
+  },
+};
+
+axios(config)
+  .then(function (response) {
+    console.log(response.data);
+    const html = response.data.results
+      .map((event) => {
+        // rounds down the rating
+        let score = event.score;
+        let roundedScore = score.toFixed(2);
+        // uses regex to remove uncessary text from city names
+        let city = event.location_id;
+        if (city) {
+          city = city.replace("2C_", " ");
+          city = city.replace(/_/g, " ");
+          city = city.replace(/-/g, " ");
+          city = city.replace(/[0-9]/g, " ");
+        }
+        return `
    <div class="user">
      <div>Site: ${event.name}</div>
      <div>City: ${city}</div>
@@ -284,12 +315,12 @@ function search() {
      <div>Description: ${event.snippet}</div>
      <div>Rating: ${roundedScore}/10.00 </div>
    </div>`;
-        })
-        .join("");
-      console.log(html);
-      document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
-    })
-    .catch((error) => {
-      console.log("error", error);
-    });
+      })
+      .join("");
+    console.log(html);
+    document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+  })
+  .catch((error) => {
+    console.log("error", error);
+  });
 }
